592b41fb7878f1f6232e48edb84ef8c1cc89dff3
han254 <peternjenga1706@gmail.com>
2022-04-24 20:25:30 +0300

    add constructors for the text and key

--- src/main/java/org/example/Encoding.java
+++ src/main/java/org/example/Encoding.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class Encoding {
+    private String plainText;
+    private int cipherKey;
+
+    public Encoding(String plainText, int cipherKey){
+        this.plainText =plainText;
+        this.cipherKey =cipherKey;
+    }
 }
