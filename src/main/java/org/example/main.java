4db6f2f948646ff4b4c0e75418dfe4a6a1e1f221
han254 <peternjenga1706@gmail.com>
2022-04-24 20:30:23 +0300

    use a scanner to print the plaintext

--- src/main/java/org/example/main.java
+++ src/main/java/org/example/main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Input the plaintext message : ");
 
     }
 }
