08ef6ed01031e206d4a9f1a4a10cf5814b63ee45
han254 <peternjenga1706@gmail.com>
2022-04-24 21:44:18 +0300

    encoding text working

--- src/main/java/org/example/Encoding.java
+++ src/main/java/org/example/Encoding.java
@@ -1,39 +1,48 @@
 package org.example;
 
+import java.util.*;
 public class Encoding {
     private String plainText;
     private int cipherKey;
 
-    public Encoding(String plainText, int cipherKey){
-        this.plainText =plainText;
-        this.cipherKey =cipherKey;
+    public Encoding(String plainText, int cipherKey) {
+        this.plainText = plainText;
+        this.cipherKey= cipherKey;
     }
-}public String getPlainText(){
-    return this.plainText;
-}
-public int getShift(){
-    return this.cipherKey;
-
-}
-
-public static String encrypt(String plainText, int cipherKey);{
-String cipherText ="";
-char alphabet;
-for(int i=0; i<plainText.length(); i++)
-{
-    alphabet =plainText.charAt(i);
-    if(alphabet>='a' && alphabet <='z'){
-        alphabet = (char) (alphabet + cipherKey);
-        if(alphabet > 'z') {
-        alphabet = (char) (alphabet+'a'-'z'-1);
-        }
-        cipherText =cipherText + alphabet;
-        }
-    else if(
-            alphabet >='A' && alphabet<='Z'){
+    public String getPlainText(){
+        return this.plainText;
     }
-        }
-        )
-        }
 
+    public int getShift(){
+        return this.cipherKey;
+    }
+    public static String encrypting(String plainText, int cipherKey ){
+        String ciphertext = "";
+        char alphabet;
+        for(int i=0; i < plainText.length();i++)
+        {
+            alphabet = plainText.charAt(i);
+            if(alphabet >= 'a' && alphabet <= 'z')
+            {
+                alphabet = (char) (alphabet + cipherKey);
+                if(alphabet > 'z') {
+                    alphabet = (char) (alphabet+'a'-'z'-1);
+                }
+                ciphertext = ciphertext + alphabet;
+            }
+            else if(alphabet >= 'A' && alphabet <= 'Z') {
+                alphabet = (char) (alphabet + cipherKey);
 
+                if(alphabet > 'Z') {
+                    alphabet = (char) (alphabet+'A'-'Z'-1);
+                }
+                ciphertext = ciphertext + alphabet;
+            }
+            else {
+                ciphertext = ciphertext + alphabet;
+            }
+
+        }
+        return ciphertext;
+    }
+}
