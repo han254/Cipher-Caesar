df0c81cf4c3a19832088f1cb81d223e1d2364349
han254 <peternjenga1706@gmail.com>
2022-04-24 22:02:21 +0300

    create a decoding class & add variables

--- src/main/java/org/example/Decoding.java
+++ src/main/java/org/example/Decoding.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Decoding {
+    private String cipherText;
+    private int shift;
+
+    public Decoding(String cipherText, int shift) {
+        this.cipherText = cipherText;
+        this.shift = shift;
 }
